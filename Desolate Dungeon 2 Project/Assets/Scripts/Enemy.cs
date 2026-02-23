f32cd6eacacdca761995a45ef4f88d0db35b9b47
andyphilipsung <andyphilips.ung@elev.ga.lbs.se>
2026-02-23 15:08:02 +0100

    Put prefab to all but not done with, damage, player healthbar and projectile

--- Desolate Dungeon 2 Project/Assets/Scripts/Enemy.cs
+++ Desolate Dungeon 2 Project/Assets/Scripts/Enemy.cs
@@ -1,16 +1,50 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    Rigidbody2D enemyRb;
+
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float checkDistance;
+
+    [SerializeField] Transform lookPosition;
+
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        RaycastHit2D hit = Physics2D.Raycast(lookPosition. position, Vector2.down, checkDistance, groundLayer);
+
+        if(hit.collider == null)
+        {
+            // flippa enemyn
+
+            transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
+        }
     }
+
+
+
+        private void FixedUpdate() 
+        {
+             MoveEnemy();
+        }
+
+
+        void MoveEnemy()
+        {
+            enemyRb.linearVelocityX = transform.right.x * moveSpeed;
+        }
+
+
+
+
 }
