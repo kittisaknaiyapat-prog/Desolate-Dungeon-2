d12467bd7dc2903d65ad3451b1fdcf2654d183a9
kittisaknaiyapat-prog <kittisak.naiyapat@elev.ga.lbs.se>
2026-02-23 13:17:23 +0100

    Re-used player scripts

---
+++ Desolate Dungeon 2 Project/Assets/All Scripts/DD_PlayerScript.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DD_PlayerScript : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D Rb;
+    public InputAction moveAction;
+    public Vector2 moveInput;
+    InputAction jumpaction;
+    private TrailRenderer trailRenderer;
+    private bool dashInput;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
+
+    [Header("Ground check system")]
+    [SerializeField] bool isGrounded;
+    [SerializeField] Transform groundCheckPosition;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
+
+    private float coyoteTime = 0.1f;
+    private float coyoteTimeCounter;
+
+    [Header("Player Status")]
+
+    [SerializeField] int PlayerHealthPoints;
+
+
+
+    [Header("Dash Related")]
+    [SerializeField] private float dashingVelocity;
+    [SerializeField] private float dashingTime;
+    private Vector2 dashDirection;
+    private bool isDashing;
+    private bool canDash = true;
+    private bool isFacingRight;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpaction = InputSystem.actions.FindAction("Jump");
+        trailRenderer = GetComponent<TrailRenderer>();
+        isFacingRight = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dashInput = Input.GetButtonDown("Dash");
+        PlayerInput();
+
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        Collider2D hit = Physics2D.OverlapCircle(groundCheckPosition.position, groundCheckRadius, groundLayer);
+        if (hit != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+
+
+
+
+    }
+
+
+    void PlayerInput()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        Rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, Rb.linearVelocity.y);
+        if (jumpaction.triggered && isGrounded)
+        {
+            Rb.linearVelocity = new Vector2(Rb.linearVelocity.x, jumpForce);
+        }
+
+
+        if (jumpaction.WasPerformedThisFrame() && isGrounded || jumpaction.WasPerformedThisFrame() && coyoteTimeCounter > 0)
+        {
+            coyoteTimeCounter = 0;
+            Rb.linearVelocityY = 0;
+            Rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (dashInput && canDash)
+        {
+
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            if (dashDirection == Vector2.zero)
+            {
+                dashDirection = new Vector2(transform.localScale.x, 0);
+            }
+            StartCoroutine(StopDashing());
+        }
+
+        if (isDashing)
+        {
+            Rb.linearVelocity = dashDirection.normalized * dashingVelocity;
+            return;
+        }
+
+        if (isGrounded)
+        {
+            canDash = true;
+        }
+
+        if (moveInput.x > 0)
+        {
+            isFacingRight = true;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        if (moveInput.x < 0)
+        {
+            isFacingRight = false;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+
+    }
+
+
+    IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+
+    }
+}
