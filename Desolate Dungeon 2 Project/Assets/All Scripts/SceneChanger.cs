4384052b98b474568496a819dad54ab6b8c3be70
kfc-consumer <anthonyrubimfelix@hotmail.com>
2026-02-23 13:38:20 +0100

    finished basic startmenu

--- Desolate Dungeon 2 Project/Assets/All Scripts/SceneChanger.cs
+++ Desolate Dungeon 2 Project/Assets/All Scripts/SceneChanger.cs
@@ -1,18 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneChanger: MonoBehaviour
 {
 
     public void PlayGame()
     {
-        
-
+        SceneManager.LoadSceneAsync(2);
     }
 
     public void OpenSettings()
     {
-        
+        SceneManager.LoadSceneAsync(1);
     }
 
 
