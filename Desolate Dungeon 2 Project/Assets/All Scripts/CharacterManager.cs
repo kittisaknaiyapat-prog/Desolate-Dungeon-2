68d1c8d820edb0ed3f9d20a6bae4a669c38aa8f1
kfc-consumer <anthonyrubimfelix@hotmail.com>
2026-02-23 14:29:06 +0100

    Skin manager

---
+++ Desolate Dungeon 2 Project/Assets/All Scripts/CharacterManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+public class CharacterManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerskin;
+
+
+
+    public void NextOption()
+    {
+        selectedSkin = selectedSkin + 1;
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        sr.sprite = skins[selectedSkin];
+
+    }
+
+    public void BackOption()
+    {
+        selectedSkin = selectedSkin - 1;
+        if (selectedSkin < 0)
+        {
+            selectedSkin = skins.Count - 1;
+        }
+        sr.sprite = skins[selectedSkin];
+
+    }
+
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerskin, "Assets/Prefabs/selectedskin.prefab");
+        SceneManager.LoadSceneAsync(2);
+    }
+
+}
