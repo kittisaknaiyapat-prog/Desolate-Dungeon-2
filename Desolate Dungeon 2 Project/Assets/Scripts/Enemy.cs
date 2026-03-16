4801072912fa6034cbf825a6281c7486d69bbf1b
kittisaknaiyapat-prog <kittisak.naiyapat@elev.ga.lbs.se>
2026-03-16 13:26:21 +0100

    Made knockback on Enemy+Player

--- Desolate Dungeon 2 Project/Assets/Scripts/Enemy.cs
+++ Desolate Dungeon 2 Project/Assets/Scripts/Enemy.cs
@@ -1,23 +1,30 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class Enemy : MonoBehaviour
 {
 
     Rigidbody2D enemyRb;
 
-    
+    public float KBF;
+    public float KBC;
+    public float KBTT;
+
+    public bool KFR;
 
     [SerializeField] float moveSpeed;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] float checkDistance;
 
+    DD_PlayerScript playerScript;
 
     [SerializeField] Transform lookPosition;
 
     void Start()
     {
         enemyRb = GetComponent<Rigidbody2D>();
+        playerScript = FindAnyObjectByType<DD_PlayerScript>();
     }
 
     void Update()
@@ -40,17 +47,58 @@ public class Enemy : MonoBehaviour
 
 
 
-        private void FixedUpdate() 
+    private void FixedUpdate() 
+    {
+        MoveEnemy();
+        EnemyKnockBack();
+
+    }
+
+
+    void MoveEnemy()
+    {
+        enemyRb.linearVelocityX = transform.right.x * moveSpeed;
+    }
+
+    void EnemyKnockBack()
+    {
+        if (KBC <= 0)
         {
-             MoveEnemy();
+            enemyRb.linearVelocity = new Vector2(enemyRb.linearVelocity.x * moveSpeed, enemyRb.linearVelocity.y);
+        }
+        else
+        {
+            if (KFR == true)
+            {
+                enemyRb.linearVelocity = new Vector2(KBF, KBF);
+            }
+            if (KFR == false)
+            {
+                enemyRb.linearVelocity = new Vector2(-KBF, KBF);
+            }
+
+            KBC -= Time.fixedDeltaTime;
         }
 
+    }
 
-        void MoveEnemy()
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
         {
-            enemyRb.linearVelocityX = transform.right.x * moveSpeed;
+          playerScript.KnockBackCounter = playerScript.KnockBackTotalTime;
+          if (collision.transform.position.x <= transform.position.x)
+          {
+              playerScript.KnockFromRight = true;
+          }
+          if (collision.transform.position.x > transform.position.x)
+          {
+                playerScript.KnockFromRight = false;
+          }
         }
-
+        enemyRb.linearVelocityY = 0;
+        enemyRb.AddForce(Vector2.up * KBF, ForceMode2D.Impulse);
+    }
 
 
 
