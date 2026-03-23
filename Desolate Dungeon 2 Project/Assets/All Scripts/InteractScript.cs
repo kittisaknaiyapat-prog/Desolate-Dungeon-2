9d00c866003002ccd7054299e635b0375256590f
kfc-consumer <anthonyrubimfelix@hotmail.com>
2026-03-23 15:05:54 +0100

    försökt fixa interact script och böjrat powerup

--- Desolate Dungeon 2 Project/Assets/All Scripts/InteractScript.cs
+++ Desolate Dungeon 2 Project/Assets/All Scripts/InteractScript.cs
@@ -1,88 +1,90 @@
-//using System.Collections;
-//using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using TMPro;
 
 
-//public class InteractScript : MonoBehaviour
-//{
-//    public GameObject dialoguePanel;
-//    public 
-//    public string[] dialogue;
-//    private int index;
 
-//    public GameObject contButton;
-//    public float wordSpeed;
-//    public bool playerIsClose;
+public class InteractScript : MonoBehaviour
+{
+    public GameObject DialoguePanel;
+    public TextMesh DialogueText;
+    public string[] Dialogue; 
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+    public bool playerIsClose;
 
  
-//    void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
-//        {
-//            if (dialoguePanel.activeInHierarchy)
-//            {
-//                zeroText();
-//            }
-//            else
-//            {
-//                dialoguePanel.SetActive(true);
-//                StartCoroutine(Typing());
-//            }
-//        }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        {
+            if (DialoguePanel.activeInHierarchy)
+            {
+                ZeroText();
+            }
+            else
+            {
+                DialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+        }
 
-//        if (dialogueText.text == dialogue[index])
-//        {
-//            contButton.SetActive(true); 
-//        }
-//    }
+        if (DialogueText.text == Dialogue[index])
+        {
+            contButton.SetActive(true); 
+        }
+    }
 
-//    public void zeroText()
-//    {
-//        dialogueText.text = " ";
-//        index = 0;
-//        dialoguePanel.SetActive(false);
-//    }
-//     IEnumerator Typing()
-//    {
-//        foreach (char letter in dialogue[index].ToCharArray())
-//        {
-//            dialogueText.text += letter;
-//            yield return new WaitForSeconds(wordSpeed);
-//        }
-//    }
+    public void ZeroText()
+    {
+        DialogueText.text = " ";
+        index = 0;
+        DialoguePanel.SetActive(false);
+    }
+     IEnumerator Typing()
+    {
+        foreach (char letter in Dialogue[index].ToCharArray())
+        {
+            DialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
 
-//    public void NextLine()
-//    {
+    public void NextLine()
+    {
 
-//        contButton.SetActive(false);
-
-
-//        if(index < dialogue.Length - 1)
-//        {
-//            index++;
-//            dialogueText.text = "";
-//            StartCoroutine(Typing());
-//        }
-//        else
-//        {
-//            zeroText();
-//        }
-//    }
+        contButton.SetActive(false);
 
 
-//    private void OnTriggerEnter2D(Collider2D other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            playerIsClose = true;
-//        }
-//    }
+        if(index < Dialogue.Length - 1)
+        {
+            index++;
+            DialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            ZeroText();
+        }
+    }
 
-//    private void OnTriggerExit2D(Collider2D other)
-//    {
-//        if (other.CompareTag("Player"))
-//        {
-//            playerIsClose = false;
-//            zeroText();
-//        }
-//    }
-//}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            ZeroText();
+        }
+    }
+}
---
+++ Desolate Dungeon 2 Project/Assets/All Scripts/PowerUpScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
