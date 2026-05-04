cfba07e887611247031305a01cdacb0ea69d8329
kfc-consumer <anthonyrubimfelix@hotmail.com>
2026-05-04 13:24:04 +0200

    audio fixat typ yes sirrrrrr

--- Desolate Dungeon 2 Project/Assets/All Scripts/SceneChanger.cs
+++ Desolate Dungeon 2 Project/Assets/All Scripts/SceneChanger.cs
@@ -3,15 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger: MonoBehaviour
 {
+    AudioController audioController;
+
+    private void Awake()
+    {
+        audioController = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioController>();
+
+
+
+    }
+
+
+
 
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(2);
+        audioController.PlaySFX(audioController.buttonclick);
     }
 
     public void OpenSettings()
     {
         SceneManager.LoadSceneAsync(1);
+        audioController.PlaySFX(audioController.buttonclick);
     }
 
 
@@ -19,6 +33,7 @@ public class SceneChanger: MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit game");
+        audioController.PlaySFX(audioController.returnbuttonclick);
     }
 
     public void ReplayGame()
