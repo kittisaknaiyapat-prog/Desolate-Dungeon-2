eacf9120785a1e87fb2036e851c7140782469b5e
kfc-consumer <anthonyrubimfelix@hotmail.com>
2026-05-22 11:29:15 +0200

    Revert "audio finished & TESTPLAYER"
    This reverts commit f8430f31a10030bfcd88116c40d0c594d849c4ca.

--- Desolate Dungeon 2 Project/Assets/Scripts/AudioController.cs
+++ Desolate Dungeon 2 Project/Assets/Scripts/AudioController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; 
 
 public class AudioController : MonoBehaviour
 {
@@ -14,58 +13,29 @@ public class AudioController : MonoBehaviour
     public AudioClip walking;
     public AudioClip buttonclick;
     public AudioClip returnbuttonclick;
-
-    public static AudioController Instance { get; private set; }
-
-    private void Awake()
+    private static AudioController instance;
+    public static AudioController GetInstance()
     {
-   
-        if (Instance != null && Instance != this)
+        return instance;
+    }
+    private void Start()
+    {
+        musicSource.clip = menumusic;
+        musicSource.Play();
+        if (instance)
         {
             Destroy(gameObject);
+
             return;
         }
 
-        Instance = this;
         DontDestroyOnLoad(gameObject);
-    }
 
-    private void OnEnable()
-    {
+        instance = this;
 
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-
-        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
- 
-        if (scene.name == "StartScene" || scene.name == "Settings 2 new")
-        {
-            PlayMusic(menumusic);
-        }
-        else
-        {
-
-            PlayMusic(background);
-        }
-    }
-
-   
-    public void PlayMusic(AudioClip clip)
-    {
-
-        if (musicSource.clip == clip) return;
-
-        musicSource.clip = clip;
-        musicSource.Play();
-    }
 
     public void PlaySFX(AudioClip clip)
     {
--- Desolate Dungeon 2 Project/Assets/Scripts/VolumeSettings.cs
+++ Desolate Dungeon 2 Project/Assets/Scripts/VolumeSettings.cs
@@ -7,50 +7,13 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
-    [SerializeField] private Slider SFXSlider; 
 
-    private void Start()
-    {
-    
-        if (myMixer.GetFloat("Music", out float musicMixerValue))
-        {
-            musicSlider.value = Mathf.Pow(10, musicMixerValue / 20);
-        }
-        musicSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
-
-    
-        if (myMixer.GetFloat("SFX", out float sfxMixerValue))
-        {
-            SFXSlider.value = Mathf.Pow(10, sfxMixerValue / 20);
-        }
-        SFXSlider.onValueChanged.AddListener(delegate { SetSFXVolume(); });
-    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 10);
 
-        if (volume <= 0.0001f)
-        {
-            myMixer.SetFloat("Music", -80f); 
-        }
-        else
-        {
-            myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
-        }
     }
-
-    public void SetSFXVolume()
-    {
-        float volume = SFXSlider.value;
-
-        if (volume <= 0.0001f)
-        {
-            myMixer.SetFloat("SFX", -80f); 
-        }
-        else
-        {
-            myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        }
-    }
+   
 }
