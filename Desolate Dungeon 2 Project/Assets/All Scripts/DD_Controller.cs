08c3efd6f636fbf1501b55f22129d9c956f5f901
kittisaknaiyapat-prog <kittisak.naiyapat@elev.ga.lbs.se>
2026-05-24 17:31:49 +0200

    test performance

--- Desolate Dungeon 2 Project/Assets/All Scripts/DD_Controller.cs
+++ Desolate Dungeon 2 Project/Assets/All Scripts/DD_Controller.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using Microsoft.Unity.VisualStudio.Editor;
+//using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
--- Desolate Dungeon 2 Project/Assets/Scripts/FlyingEnemy.cs
+++ Desolate Dungeon 2 Project/Assets/Scripts/FlyingEnemy.cs
@@ -1,47 +1,47 @@
-using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
-using UnityEngine;
+//using Unity.VisualScripting;
+//using UnityEditor.Tilemaps;
+//using UnityEngine;
 
-public class FlyingEnemy : MonoBehaviour
-{
+//public class FlyingEnemy : MonoBehaviour
+//{
 
 
-    public GameObject player;
-    private Transform playerRb;
-    private Vector3 currentPos;
-    public float distance;
-    public float speed;
+//    public GameObject player;
+//    private Transform playerRb;
+//    private Vector3 currentPos;
+//    public float distance;
+//    public float speed;
     
-    void Start()
-    {
-        playerRb = player.GetComponent<Transform>();
-        currentPos = GetComponent<Transform>().position;
-    }
+//    void Start()
+//    {
+//        playerRb = player.GetComponent<Transform>();
+//        currentPos = GetComponent<Transform>().position;
+//    }
    
 
-    void Update()
-    {
-        if (Vector2.Distance(transform.position, playerRb.position) < distance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, playerRb.position, speed * Time.deltaTime);
-        }
-        else
-        {
-            if (Vector2.Distance(transform.position, currentPos) <=0)
-            {
+//    void Update()
+//    {
+//        if (Vector2.Distance(transform.position, playerRb.position) < distance)
+//        {
+//            transform.position = Vector2.MoveTowards(transform.position, playerRb.position, speed * Time.deltaTime);
+//        }
+//        else
+//        {
+//            if (Vector2.Distance(transform.position, currentPos) <=0)
+//            {
 
-            }
-            else
-            {
-                transform.position = Vector2.MoveTowards(transform.position, currentPos, speed * Time.deltaTime);
+//            }
+//            else
+//            {
+//                transform.position = Vector2.MoveTowards(transform.position, currentPos, speed * Time.deltaTime);
                  
-            }
+//            }
 
-        }
+//        }
 
-    }
-
-
+//    }
 
 
-}
+
+
+//}
